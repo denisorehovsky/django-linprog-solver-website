4836a4778991854490e65ac30a667e19e8682d22
Denis Orehovsky <denis.orehovsky@gmail.com>
2017-04-15 12:36:52 +0300

    SimplexSolveView returns error message if get request is invalid

--- linprog_solver/simplex/views.py
+++ linprog_solver/simplex/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import TemplateView, FormView
+from django.contrib import messages
+from django.views.generic import FormView
 
-from .forms import SimplexInitForm
+from .exceptions import SimplexInitException
+from .forms import SimplexInitForm, SimplexSolveForm
 
 
 class SimplexInitView(FormView):
@@ -8,9 +10,20 @@ class SimplexInitView(FormView):
     form_class = SimplexInitForm
 
 
-class SimplexSolveView(TemplateView):
+class SimplexSolveView(FormView):
     template_name = 'simplex/simplex_solve.html'
+    form_class = SimplexSolveForm
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update({'variables': self.request.GET.get('variables', None)})
+        kwargs.update({'conditions': self.request.GET.get('conditions', None)})
+        return kwargs
 
     def get(self, request, *args, **kwargs):
-        context = self.get_context_data(**kwargs)
-        return self.render_to_response(context)
+        try:
+            return super().get(request, *args, **kwargs)
+        except SimplexInitException as error:
+            messages.add_message(request, messages.ERROR, str(error))
+            kwargs.update({'form': None})
+            return self.render_to_response(self.get_context_data(**kwargs))
--- tests/unit/simplex/test_views.py
+++ tests/unit/simplex/test_views.py
@@ -1,6 +1,6 @@
 from test_plus.test import TestCase
 
-from linprog_solver.simplex.forms import SimplexInitForm
+from linprog_solver.simplex.forms import SimplexInitForm, SimplexSolveForm
 
 
 class TestSimplexInitView(TestCase):
@@ -25,3 +25,21 @@ class TestSimplexSolveView(TestCase):
                                                'is_non_negative': 'on'})
         self.response_200(resp)
         self.assertTemplateUsed(resp, 'simplex/simplex_solve.html')
+        self.assertIsInstance(resp.context['form'], SimplexSolveForm)
+
+    def test_get_with_bad_data(self):
+        resp = self.get('simplex:solve', data={'variables': 'should_be_num',
+                                               'conditions': 4,
+                                               'is_non_negative': 'on'})
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'simplex/simplex_solve.html')
+        self.assertEqual(str(list(resp.context['messages'])[0]),
+                         'Please define the number of variables and conditions')
+
+        resp = self.get('simplex:solve', data={'variables': 15,
+                                               'conditions': 4,
+                                               'is_non_negative': 'on'})
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'simplex/simplex_solve.html')
+        self.assertEqual(str(list(resp.context['messages'])[0]),
+                         'The number of variables and conditions should be between 1 and 10')
