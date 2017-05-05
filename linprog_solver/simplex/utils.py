e1da0635fbe8539e168ae59c49c31aee0b73d9bd
Denis Orehovsky <denis.orehovsky@gmail.com>
2017-05-05 10:24:42 +0300

    Refactor generate_latex_result

--- linprog_solver/simplex/utils.py
+++ linprog_solver/simplex/utils.py
@@ -3,9 +3,9 @@ import sympy
 from .constants import STATUS
 
 
-def generate_latex_result(function_coefficients, tendency, constraints, solution):
+def generate_latex_result(function_coefficients, tendency, constraints, result):
     """
-    Dummy code to generate latex result of solved linear programming problem.
+    Dummy code that generates latex result of solved linear programming problem.
 
     TODO: Find better solution.
     """
@@ -17,23 +17,23 @@ def generate_latex_result(function_coefficients, tendency, constraints, solution
         sympy.symbols('{}'.format(tendency))
     ))
 
-    result = sympy.latex(function) + '\\\\[0.3in]'
+    latex = sympy.latex(function) + '\\\\[0.3in]'
 
-    result += '\\begin{cases}'
+    latex += '\\begin{cases}'
     inequalities = [sympy.Rel(sum(coeff * variable for coeff, variable in zip(coeffs, variables)), const, operator)
                     for coeffs, operator, const in constraints]
     for inequality in inequalities:
-        result += sympy.latex(inequality) + r'\\'
-    result += '\\end{cases}'
+        latex += sympy.latex(inequality) + r'\\'
+    latex += '\\end{cases}'
 
-    result += '\\\\' + sympy.latex(sympy.Rel(sympy.symbols('X'), 0, '>='))
+    latex += '\\\\' + sympy.latex(sympy.Rel(sympy.symbols('X'), 0, '>='))
 
-    result += '\\\\[0.3in] \\text{%s}' % STATUS[solution['status']]
+    latex += '\\\\[0.3in] \\text{%s}' % STATUS[result['status']]
 
-    if solution['success']:
-        result += '\\\\[0.2in]' + sympy.latex(sympy.Eq(F, solution['fun']))
+    if result['success']:
+        latex += '\\\\[0.2in]' + sympy.latex(sympy.Eq(F, result['fun']))
 
-        for x_value in [sympy.Eq(variable, value) for variable, value in zip(variables, solution['x'])]:
-            result += sympy.latex(r'\\') + sympy.latex(x_value)
+        for x_value in [sympy.Eq(variable, value) for variable, value in zip(variables, result['x'])]:
+            latex += sympy.latex(r'\\') + sympy.latex(x_value)
 
-    return result
+    return latex
